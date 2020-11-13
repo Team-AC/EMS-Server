e438ec2803d09c90cd4e393ec75a4201fe764a82
Basil Mahmood <basilmahmood98@gmail.com>
2020-11-12 19:05:18 -0500

    Added in basic socket.io setup

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
-// Config
+// --- Config --- //
+
+// dotenv
 require('dotenv').config();
 
+// Express
 const express = require('express');
 const app = express();
 const port = 3000;
-
 app.use(express.json({limit: "5mb"}));
 
+// MongoDB
 require('./config/mongo');
 
+// Socket.io
+const server = require('http').createServer(app);
+const options = { };
+const io = require('socket.io')(server, options);
+io.on('connection', socket => { 
+  socket.on('message', data => {
+    console.log(data);
+  });
+ });
+
+
+// Cors
 const cors = require('cors');
 app.use(cors());
 
-// Routes
+// --- Routes ---//
 const api = require('./api/api');
 
 app.use('/api', api);
 
-// Server
+// --- Server --- //
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 })
+
 
