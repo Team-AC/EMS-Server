39f398fed43afb671815e05b137c1094d5ea8a58
Basil Mahmood <basilmahmood98@gmail.com>
2020-11-13 19:38:47 -0500

    Added socket event for new murb power

--- src/sockets/sockets.js
+++ src/sockets/sockets.js
@@ -5,5 +5,11 @@ module.exports = (socket) => {
     murbPower.create(data, (err) => {
       if (err) console.error(err);
     });
-  })
+  });
+
+  socket.on("New Murb Power", (data) => {
+    murbPower.create(data, (err) => {
+      if (err) console.error(err);
+    });
+  });
 }
