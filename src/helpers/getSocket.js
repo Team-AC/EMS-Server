382d21643780437fc01d69e508645a77c777109d
Basil Mahmood <basilmahmood98@gmail.com>
2020-11-22 22:03:58 -0500

    Added small comment to helper function

--- src/helpers/getSocket.js
+++ src/helpers/getSocket.js
@@ -1,3 +1,4 @@
+// Returns a socket from a server instance (io), with socket.io v2 syntax
 module.exports = (io) => {
   const sockets = io.sockets.connected;
   return sockets[Object.keys(sockets)[0]];
