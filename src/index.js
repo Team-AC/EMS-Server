12401d060fede5fc9721f1ab2a290fc93d2f7400
Basil Mahmood <basilmahmood98@gmail.com>
2021-01-07 20:55:26 -0500

    Added code to test production build

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 // Express
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = parseInt(process.env.PROD) ? 80 : 3000; // Use port 80 in production, 3000 in development
 app.use(express.json({limit: "5mb"}));
@@ -30,6 +31,10 @@ io.on('connection', socket => {
 const api = require('./api/api')(io);
 
 app.use('/api', api);
+
+//** USE ONLY TO TEST PRODUCTION BUILD */
+// app.use('/', express.static(path.join(__dirname, '../../EMS-App', 'build')));
+// app.use('*', express.static(path.join(__dirname, '../../EMS-App', 'build')));
 
  // Listening
 server.listen(port, () => {
