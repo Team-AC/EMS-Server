30e90ab8d304b7af0e079599c5e99bd7290575b7
Basil Mahmood <basilmahmood98@gmail.com>
2021-01-11 18:12:57 -0500

    Added interval to evApi

--- src/api/evApi.js
+++ src/api/evApi.js
@@ -4,12 +4,13 @@ const getSocket = require('../helpers/getSocket');
 const evAPI = express.Router();
 
 module.exports = (io) => {
-  evAPI.post('/generate', (req, res) => {
+  evAPI.post('/generate/:interval', (req, res) => {
+    const { interval } = req.params;
     const evParameters = req.query;
     
     const socket = getSocket(io);
   
-    socket.emit("Generate Ev", evParameters);
+    socket.emit("Generate Ev", interval, evParameters);
 
     res.sendStatus(200);
   
