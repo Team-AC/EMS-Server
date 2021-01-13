b35a5181079532cd2f483d3888573f80b96f84ac
Basil Mahmood <basilmahmood98@gmail.com>
2021-01-13 16:41:41 -0500

    Added a get request for evapi

--- src/api/evApi.js
+++ src/api/evApi.js
@@ -1,14 +1,34 @@
 const { subDays } = require('date-fns');
 const express = require('express');
+const getCostFromPower = require('../helpers/getCostFromPower');
+const getOffPeakUsage = require('../helpers/getOffPeakUsage');
+const getPeakUsage = require('../helpers/getPeakUsage');
 const getSocket = require('../helpers/getSocket');
-const { evPower } = require('../models/ev');
+const { evPower, evPowerDaily, evPowerWeekly, evPowerMonthly } = require('../models/ev');
 const preAddEvPower = require('../services/preAddEvPower');
 const removeAllEv = require('../services/removeAllEv');
 
 const evAPI = express.Router();
 
+function validateInterval(req, res, next) {
+  const { interval } = req.params;
+  const possibleValues = [
+    "pastDay",
+    "pastWeek",
+    "pastMonth",
+    "past3Months",
+    "pastYear"
+  ];
+
+  if (possibleValues.includes(interval)) {
+    next();
+  } else {
+    res.status(400).send(`The interval parameter ${interval} is not valid`);
+  }
+}
+
 module.exports = (io) => {
-  evAPI.post('/generate/:interval', (req, res) => {
+  evAPI.post('/generate/:interval', validateInterval, (req, res) => {
     const { interval } = req.params;
     const evParameters = req.query;
     
@@ -45,6 +65,70 @@ module.exports = (io) => {
         res.status(400).send("Cannot generate data while there is still data in the database, send the delete request first")
       }
     });
+  });
+
+  evAPI.get('/:interval', validateInterval, (req, res) => {
+    const { interval } = req.params;
+    
+    const amountOfDaysToSub = {
+      "pastDay": 1,
+      "pastWeek": 7,
+      "pastMonth": 30,
+      "past3Months": 90,
+      "pastYear": 365
+    }
+
+    const model = {
+      "pastDay": evPower,
+      "pastWeek": evPowerDaily,
+      "pastMonth": evPowerDaily,
+      "past3Months": evPowerWeekly,
+      "pastYear": evPowerMonthly
+    }
+
+    model[interval].find({
+      TimeStamp: {
+        $gte: subDays(new Date(), amountOfDaysToSub[interval]),
+        $lte: new Date()
+      }
+    }, (err, data) => {
+      const aggregatedData = [];
+
+      if (interval === "pastDay") {
+        data.forEach(data => {
+          aggregatedData.push({
+            ...data,
+            Cost: getCostFromPower(data.Power, data.TimeStamp),
+          })
+        })
+      }
+
+      data.forEach(({TimeStamp, Power, TotalPower, AggregatedAmount, ...restData}) => {
+        if (TotalPower) {
+          aggregatedData.push({
+            TimeStamp,
+            TotalPower,
+            AggregatedAmount,
+            AveragePowerPerEv: TotalPower/AggregatedAmount,
+            Cost: getCostFromPower(TotalPower, TimeStamp),
+            ...restData
+          })
+        }
+      });
+      
+      const peakUsage = getPeakUsage(aggregatedData);
+      const offPeakUsage = getOffPeakUsage(aggregatedData);
+
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        res.send({
+          peakUsage,
+          offPeakUsage,
+          aggregatedData
+        });
+      }
+    }).lean();
   });
 
   evAPI.delete('/', (req, res) => {
