6f5df1f8ed931250c188a7022a4fc2db37adfedc
Basil Mahmood <basilmahmood98@gmail.com>
2021-02-03 19:04:37 -0500

    Added bess api

---
+++ src/api/bessApi.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const getSocket = require('../helpers/getSocket');
+
+const bessApi = express.Router();
+
+module.exports = (io) => {
+  bessApi.post('/init', (req, res) => {
+    const socket = getSocket(io);
+
+    const bessParams = req.query;
+
+    socket.emit("Bess Init", bessParams, () => {
+      res.sendStatus(200)
+    })
+  })
+
+  bessApi.post('/charge', (req, res) => {
+    const socket = getSocket(io);
+
+    socket.emit("Bess Charge", () => {
+      res.sendStatus(200)
+    })
+  })
+
+  return bessApi
+}
